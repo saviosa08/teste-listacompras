f56db7a5eee491b4f121cf722677ca40ed60d39e
Sávio Sant'anna <saviosantannadesa@gmail.com>
2024-01-10 10:31:59 -0300

    inclusao projeto

---
+++ lista-de-compras/src/app/componentes/itens-lista/itens-lista.component.ts
@@ -0,0 +1,60 @@
+import { NovoItemComponent } from './../novo-item/novo-item.component';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { ItensService } from '../itens.service';
+import { Item } from './../item';
+
+@Component({
+  selector: 'app-itens-lista',
+  templateUrl: './itens-lista.component.html',
+  styleUrls: ['./itens-lista.component.css'],
+})
+export class ItensListaComponent implements OnInit {
+  listaItens: Item[] = [];
+  listados: boolean = false; //alterar para false
+  comprados: boolean = false; //alterar para false
+  //@Output() comprados: EventEmitter<boolean> = new EventEmitter()
+  existe: boolean = true;
+  numItens: number = 0;
+
+  constructor(private service: ItensService) {}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((itens) => {
+      this.listaItens = itens;
+      this.numItens = itens.length;
+      this.verificar();
+    });
+  }
+
+  verificar() {
+    this.existe = true;
+    this.comprados = false;
+    this.listaItens.forEach((e) => {
+      if (this.existe && e.check) {
+        this.existe = false;
+        this.comprados = true;
+      }
+    });
+    this.existe = true;
+    this.listados = false;
+    this.listaItens.forEach((e) => {
+      if (this.existe && !e.check) {
+        this.existe = false;
+        this.listados = true;
+        this.atualiza();
+      }
+    });
+  }
+
+  atualiza() {
+    this.service.listar().subscribe();
+  }
+}
